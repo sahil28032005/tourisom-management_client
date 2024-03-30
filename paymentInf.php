f92a58cf2652f2eb009be71e9bcab4b41296b681
$@#il <sahilsadekar249775@gmail.com>
2024-03-31 01:26:26 +0530

    created payement inf details gateway

--- paymentInf.php
+++ paymentInf.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         //at this point we have come means payment has been done succesfully otherwise we not come here
         $identifier = $_POST['identifier'];
         $paymentId = $_POST['paymentId'];
-        $query = "SELECT date, user, package, email, persons FROM userdetails WHERE no = '$identifier'";
+        $query = "SELECT date, user, package, email, persons,`payable amount` FROM userdetails WHERE no = '$identifier'";
         $result = mysqli_query($con, $query);
         if ($result) {
             // Fetch associative array (key-value pairs) of the first row
@@ -19,9 +19,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $package = $row['package'];
                 $email = $row['email'];
                 $persons = $row['persons'];
+                $amtPaid = $row['payable amount'];
                 // Now $name, $value, and $age contain the data from the database
                 // You can use these variables as needed in your PHP code
-                echo "username: $user, package: $package, email: $email,persons:$persons";
+                echo "username: $user, package: $package, email: $email,persons:$persons,payable amount: $amtPaid";
+                $sql = "INSERT INTO payment_information(`payment id`, username, `amount paid`,peoples, packagename, email) VALUES ('$paymentId', '$user', '$amtPaid','$persons','$package','$email')";
+                $result = mysqli_query($con, $sql);
+                if ($result) {
+                    echo "payment details added successfully";
+                } else {
+                    echo "payment details not added successfully...";
+                }
             } else {
                 echo "No data found in the table.";
             }
